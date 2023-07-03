712e861a4c7ec5d752d262d4b0da21b210f70198
Samson Tush <samo4421@icloud.com>
2023-07-03 18:54:44 +0300

    Add tailwind css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up) do |u|
-          u.permit(:name, :email, :password)
-        end
-        devise_parameter_sanitizer.permit(:account_update) do |u|
-          u.permit(:name, :email, :password, :password_confirmation, :current_password)
-        end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password)
     end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, only: %i[index]
+  before_action :authenticate_user!, only: %i[index]
 
-    def index
-        @users = User.all
-    end
+  def index
+    @users = User.all
+  end
 end
