29e325b2a8cc2017364ae1cf652f5690856f05de
fuadmoin <fuadamoin@gmail.com>
2023-07-07 16:32:37 +0300

    Add signout option.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   root "recipes#index"
   resources :users, only: %i[index]
   resources :foods, only: %i[index show new create edit update destroy]
