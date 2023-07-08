e5956605427d6b070081b4d55f88d35bb0b2e23c
Samson Tush <samo4421@icloud.com>
2023-07-08 14:34:11 +0300

    Fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,9 +1,9 @@
 class Food < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-    has_many :food_recipes, dependent: :destroy
-    has_many :recipes, through: :food_recipes
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :food_recipes, dependent: :destroy
+  has_many :recipes, through: :food_recipes
 
-    validates :name, presence: true
-    validates :price, numericality: {greater_than: 0}
-    validates :quantity, numericality: {greater_than: 0}
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { greater_than: 0 }
 end
