df3e16e24913dd68b5ea398c549cea7522bbd611
fuadmoin <fuadamoin@gmail.com>
2023-07-08 12:28:52 +0300

    Fix linter errors.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,8 @@
 class RecipesController < ApplicationController
-    before_action :authenticate_user!, only: %i[index]
-  
-    def index
-      @recipes = Recipe.all
-      @user = current_user
-    end
+  before_action :authenticate_user!, only: %i[index]
+
+  def index
+    @recipes = Recipe.all
+    @user = current_user
+  end
 end
