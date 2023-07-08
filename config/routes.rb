00153e7a1ac83c6ed902ae5e8df595f664f110f6
Samson Tush <samo4421@icloud.com>
2023-07-08 14:05:39 +0300

    Add general shopping

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "users#index"
-  resources :users, only: %i[index ]
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+  root "recipes#index"
+  resources :users, only: %i[index]
+  resources :foods, only: %i[index show new create edit update destroy]
+  resources :recipes, only: %i[index show new create edit update destroy] do
+    resources :food_recipes, only: [:create, :edit, :update, :destroy]
+    member do
+      patch :update_public_status
+    end
+    get :general_shopping_list, on: :member
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,37 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_134229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price"
+    t.integer "quantity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "foods_recipes", id: false, force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
+    t.float "quantity"
+    t.index ["food_id", "recipe_id"], name: "index_foods_recipes_on_food_id_and_recipe_id"
+    t.index ["recipe_id", "food_id"], name: "index_foods_recipes_on_recipe_id_and_food_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.integer "preparation_time"
+    t.integer "cooking_time"
+    t.string "description"
+    t.boolean "public"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
@@ -31,4 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_134229) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users"
 end
