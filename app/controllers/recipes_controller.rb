d8a344838fc7bef38ebc542672bade6d722c3b27
fuadmoin <fuadamoin@gmail.com>
2023-07-08 12:15:59 +0300

    Create a recipe table.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,8 @@
+class RecipesController < ApplicationController
+    before_action :authenticate_user!, only: %i[index]
+  
+    def index
+      @recipes = Recipe.all
+      @user = current_user
+    end
+end
