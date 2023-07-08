b1bde10899b099ebf6f95f3af092579ebe7ece6e
Samson Tush <samo4421@icloud.com>
2023-07-08 13:02:11 +0300

    Fix linter files

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,34 +1,31 @@
 class FoodsController < ApplicationController
-    before_action :authenticate_user!, only: %i[index]
-    
-    def index
-        @user = current_user
-        @foods = @user.foods.all
+  before_action :authenticate_user!, only: %i[index]
+
+  def index
+    @user = current_user
+    @foods = @user.foods.all
+  end
+
+  def create
+    @user = current_user
+    @food = @user.foods.build(post_params)
+
+    if @food.save
+      redirect_to @food
+    else
+      render 'new'
     end
-    
-    def create 
-        @user = current_user
-        @food = @user.foods.build(post_params)
-    
-        if @food.save
-        redirect_to @food
-        else
-        render 'new'
-        end
-    end
-    
-    
-    def destroy
-        @food = Food.find(params[:id])
-        @food.destroy
-        redirect_to foods_path, notice: 'Food was successfully destroyed'
-    end
-    
-    private
-    
-    def post_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-    end
-    
-    
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path, notice: 'Food was successfully destroyed'
+  end
+
+  private
+
+  def post_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
