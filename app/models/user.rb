b3d9d4546edd8d78bc62a8d61c48b1c7ebbd5516
fuadmoin <fuadamoin@gmail.com>
2023-07-15 04:18:43 +0300

    Deploy on render.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :foods, foreign_key: 'user_id', dependent: :destroy
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
 end
